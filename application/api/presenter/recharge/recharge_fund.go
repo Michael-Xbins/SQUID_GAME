8201bf3be466ac09681ce1fbeb625d1e510f7d79
sky <644076264@qq.com>
2024-12-29 23:55:44 +0800

    fix

--- application/api/presenter/recharge/recharge_fund.go
+++ application/api/presenter/recharge/recharge_fund.go
@@ -5,8 +5,6 @@ type Fund struct {
 	UsdtPool        int64 `bson:"usdtPool"`        // 无上线,无上上线 usdt流向库存(用于对账)
 	ServiceFee      int64 `bson:"serviceFee"`      // 提现手续费库存(用于对账)
 	WithdrawnAmount int64 `bson:"withdrawnAmount"` // 累计已提现金额
-	UsdtToSquCoins  int64 `bson:"usdtToSquCoins"`  // 花费usdt兑换游戏币
-	SquToUsdtCoins  int64 `bson:"squToUsdtCoins"`  // 花费游戏币兑换usdt
 	ExchangeCoins   int64 `bson:"exchangeCoins"`   // 游戏币闪兑
 }
 
